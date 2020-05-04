8641616715401226740c10ba75b7c4140f45a7d9
baha908 <baho908@gmail.com>
2020-05-04 17:52:06 +0300

    update

--- YMT/projects/SingletonForm.Designer.cs
+++ YMT/projects/SingletonForm.Designer.cs
@@ -29,6 +29,7 @@
 		private void InitializeComponent()
 		{
 			this.button1 = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
 			this.SuspendLayout();
 			// 
 			// button1
@@ -41,11 +42,20 @@
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(249, 195);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(280, 243);
+			this.richTextBox1.TabIndex = 1;
+			this.richTextBox1.Text = "";
+			// 
 			// SingletonForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.richTextBox1);
 			this.Controls.Add(this.button1);
 			this.Name = "SingletonForm";
 			this.Text = "SingletonForm";
@@ -56,5 +66,6 @@
 		#endregion
 
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
 	}
 }
--- YMT/projects/SingletonForm.cs
+++ YMT/projects/SingletonForm.cs
@@ -20,8 +20,39 @@ namespace YMT.projects
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			Test test = new Test("test");
-			test.ShowText();
+			List<Tuple<String, String>> liste = new List<Tuple<string, string>>();
+			List<Tuple<int, String>> adaylar = new List<Tuple<int, String>>{
+				new Tuple<int, string>( 2, "Onur"),
+				new Tuple<int, string>( 2, "Hakan"),
+				new Tuple<int, string>( 2, "Hasan"),
+				new Tuple<int, string>( 3, "Bahadır"),
+			};
+			List<String> gorevler = new List<String> {
+				"AbstractFactory",
+				"Adapter",
+				"Bridge",
+				"Builder",
+				"Facade",
+				"Factory",
+				"Prototype",
+				"Proxy",
+				"Singleton",
+			};
+			foreach (var aday in adaylar)
+			{
+				for (int i = 0; i < aday.Item1; i++)
+				{
+					gorevler = gorevler.OrderBy(a => Guid.NewGuid()).ToList();
+					liste.Add(new Tuple<string, string>(aday.Item2, gorevler[0])); ;
+					gorevler.RemoveAt(0);
+				}
+			}
+
+			richTextBox1.Clear();
+			foreach (var item in liste)
+			{
+				richTextBox1.AppendText(item.Item1 + " - " + item.Item2 + "\n");
+			}
 		}
 	}
 }
