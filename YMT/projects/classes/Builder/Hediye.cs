2f4a516ac0d08948e5ba438868eff24c16e7a467
HASAN SOFİOĞLU <182802034@ogr.cbu.edu.tr>
2020-05-22 02:37:10 +0300

    Builder calss 1 eklendi

---
+++ YMT/projects/classes/Builder/Hediye.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMT.projects.classes.Builder
+{
+	class Hediye
+	{
+         public string HediyeUrun { get; set; }
+    public int Kodu { get; set; }
+    public string Kazanan { get; set; }
+    public override string ToString()
+    {
+        System.Windows.Forms.MessageBox.Show($"{Kodu} numaralı {HediyeUrun} promosyonu {Kazanan}  kazanmıştır.");
+        return "";
+    }
+	}
+}
