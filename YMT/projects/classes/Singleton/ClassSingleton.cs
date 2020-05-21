7ae7461d56f14666c088c4bf7dba6fcf2f0ae05e
HASAN SOFİOĞLU <182802034@ogr.cbu.edu.tr>
2020-05-21 03:22:46 +0300

    Singleton bitti

---
+++ YMT/projects/classes/Singleton/ClassSingleton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMT
+{
+    class ClassSingleton
+    {
+        public static ClassSingleton BoxName;
+        private  ClassSingleton()
+        {
+
+        }
+        public static ClassSingleton boxName()
+        {
+            if (BoxName == null)
+                BoxName = new ClassSingleton();
+            return BoxName;
+        }
+        public string box(string girilendeger)
+        {
+            Dictionary<string, string> boxcode = new Dictionary<string, string>();
+            boxcode.Add("1", "Kulaklık");
+            boxcode.Add("2", "Hoparlör");
+            boxcode.Add("3", "Mouse");
+            boxcode.Add("4", "Kasa");
+            boxcode.Add("5", "Monitor");
+            boxcode.Add("6", "Ekran Kartı");
+            
+            try
+            {
+              string deger = boxcode[girilendeger];
+                return deger;
+            }
+            catch
+            {
+                return "Girilen Koda ait kutu yok";
+            }
+             
+        }
+
+    }
+}
